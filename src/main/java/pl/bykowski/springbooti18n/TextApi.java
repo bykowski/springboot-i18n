a14c2d9a366642216d3cf4cc4daae53d5f050a79
Przemyslaw Bykowski <p.bykowski@sdacademy.pl>
2019-04-13 10:42:14 +0200

    params

--- src/main/java/pl/bykowski/springbooti18n/TextApi.java
+++ src/main/java/pl/bykowski/springbooti18n/TextApi.java
@@ -1,10 +1,11 @@
 package pl.bykowski.springbooti18n;
 
 
-import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Locale;
@@ -32,6 +33,14 @@ public class TextApi {
     @GetMapping("/niemiecki")
     public String getDE() {
         return messageSource.getMessage("hello", null, Locale.GERMAN);
+    }
+
+
+    @GetMapping("/sayHello")
+    public String sayHello(@RequestHeader(defaultValue = "PL", required = false) String language,
+                           @RequestParam String name,
+                           @RequestParam String surname) {
+        return messageSource.getMessage("hello", new Object[]{name, surname}, Locale.forLanguageTag(language));
     }
 
 }
